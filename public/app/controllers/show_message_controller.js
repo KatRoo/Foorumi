5880535fd47e371b561a63d09e7887575ceb3d98
Katri Roos <katri.roos@cs.helsinki.fi>
2015-08-24 13:55:38 +0300

    Tehtava 53 tehty

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,3 +1,22 @@
-FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('ShowMessageController', function ($scope, $routeParams, Api) {
+    $scope.newReplyContent = "";
+
+    Api.getMessage($routeParams.id).
+            success(function (data, status, headers, config) {
+                $scope.viesti = data;
+            })
+            .error(function (data, status, headers, config) {
+                console.log('Viestin haku ei onnistu.');
+            });
+
+    $scope.addReply = function () {
+        Api.addMessage({"content": $scope.newReplyContent}, $scope.viesti.id).
+                success(function (data, status, headers, config) {
+                    $scope.viesti.Replies.push(data);
+                })
+                .error(function (data, status, headers, config) {
+                    console.log('Vastauksen lisäys ei onnistu.');
+                });
+    }
+
 });
--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,4 +1,7 @@
 FoorumApp.controller('ShowTopicController', function ($scope, $routeParams, $location, Api) {
+    $scope.newTitle = "";
+    $scope.newContent = "";
+    
     Api.getTopic($routeParams.id).
             success(function (data, status, headers, config) {
                 $scope.aihe = data;
@@ -6,4 +9,14 @@ FoorumApp.controller('ShowTopicController', function ($scope, $routeParams, $loc
             .error(function (data, status, headers, config) {
                 console.log('Aiheen haku ei onnistu.');
             });
+
+    $scope.addMessage = function () {
+        Api.addMessage({"title": $scope.newTitle, "content": $scope.newContent}, $scope.aihe.id).
+                success(function (data, status, headers, config) {
+                    $location.path('/messages/' + data.id);
+                })
+                .error(function (data, status, headers, config) {
+                    console.log('Viestin lisäys ei onnistu.');
+                });
+    }
 });
